d0e7b70625af0db59eab4394f1c2f678cea23fa0
gotelloc <106339839+gotelloc@users.noreply.github.com>
2024-05-03 16:43:39 -0400

    Update HomeController.java - Prueba

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@RequestMapping("/")
-	public @ResponseBody String greeting() {
-		return "Hola, soy Gonzalo Tello";
-	}
+    @RequestMapping
+    @ResponseBody
+    public String greeting() {
+        return "Hola, soy Gonzalo Tello";
+    }
 
 }
