eaab39fa447c2d8b126502e28eae3db556f30a30
gotelloc <106339839+gotelloc@users.noreply.github.com>
2024-05-03 17:12:24 -0400

    Importa la clase Autowired

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.testingweb;
 
+import org.springframework.beans.factory.annotation.Autowired; // Importa la clase Autowired
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
