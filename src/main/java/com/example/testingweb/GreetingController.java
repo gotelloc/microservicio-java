2b385b84d1fe05262d420bc980762a1fba6f6683
gotelloc <106339839+gotelloc@users.noreply.github.com>
2024-05-03 17:15:47 -0400

    Update GreetingController.java - volver al 75%

--- src/main/java/com/example/testingweb/GreetingController.java
+++ src/main/java/com/example/testingweb/GreetingController.java
@@ -1,6 +1,5 @@
 package com.example.testingweb;
 
-import org.springframework.beans.factory.annotation.Autowired; // Importa la clase Autowired
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,12 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GreetingController {
 
-    @Autowired
-    private GreetingService service;
+	private final GreetingService service;
 
-    @RequestMapping("/greeting")
-    public @ResponseBody String greeting() {
-        return service.greet();
-    }
+	public GreetingController(GreetingService service) {
+		this.service = service;
+	}
+
+	@RequestMapping("/greeting")
+	public @ResponseBody String greeting() {
+		return service.greet();
+	}
 
 }
