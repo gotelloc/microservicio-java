7ebe94b624e2d510c5df75630556db7eaf833a02
gotelloc <106339839+gotelloc@users.noreply.github.com>
2024-05-03 18:03:47 -0400

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -2,17 +2,9 @@ package com.example.testingweb;
 
 import org.springframework.stereotype.Service;
 
-/**
- * Servicio que proporciona un saludo simple.
- */
 @Service
 public class GreetingService {
-
-    /**
-     * Retorna un saludo simple.
-     * @return El saludo
-     */
-    public String greet() {
-        return "Hola, soy Gonzalo Tello";
-    }
+	public String greet() {
+		return "Hello, World";
+	}
 }
